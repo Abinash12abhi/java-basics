f0f48bd44dcf0cb3910763cbd8de1caf892c0fc8
Abinash12abhi <118423378+Abinash12abhi@users.noreply.github.com>
2023-02-08 21:35:24 +0530

    Profit
    Input consists of 2 floating point numbers which correspond to X and Y.
    
    Refer sample input and output for formatting specifications. .
    
     Sample Input1:
    
    60.0
    
    4
    
    Sample Output1:
    
    Enter the price of a dozen mangoes
    
    Enter the price at which 1 mango is being sold
    
    Loss : Rs.12.00
    
    Sample Input 2:
    
    60.0
    
    6
    
    Sample Output 2:
    
    Enter the price of a dozen mangoes
    
    Enter the price at which 1 mango is being sold
    
    Profit : Rs.12.00
    
    Sample Input 3:
    
    60.0
    
    5
    
    Sample Output 3:
    
    Enter the price of a dozen mangoes
    
    Enter the price at which 1 mango is being sold
    
    No profit nor loss

---
+++ Profit.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+public class Profit
+{
+    public static void main(String args[])
+    {
+        float a,b,p,sum,total;
+        Scanner sc=new Scanner(System.in);
+        a=sc.nextFloat();
+        b=sc.nextFloat();
+        p=a/12;
+        if(p>b)
+        {
+            total=(p-b)*12;
+            System.out.println("Enter the price of a dozen mangoes");
+            System.out.println("Enter the price at which 1 mango is being sold");
+            System.out.printf("Loss : Rs.%.2f",total);
+        }
+        else if(p==b)
+        {
+             total=(p-b)*12;
+            System.out.println("Enter the price of a dozen mangoes");
+            System.out.println("Enter the price at which 1 mango is being sold");
+            System.out.println("No profit nor loss");
+        }
+        else
+        {
+            total=(b-p)*12;
+            System.out.println("Enter the price of a dozen mangoes");
+            System.out.println("Enter the price at which 1 mango is being sold");
+            System.out.printf("Profit : Rs.%.2f",total);
+        }
+    }
+}
