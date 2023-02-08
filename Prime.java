37cda4fafef1b28a5cc431ff7c67570ea642bb77
Abinash12abhi <118423378+Abinash12abhi@users.noreply.github.com>
2023-02-08 22:04:57 +0530

    Prime
    Write a program to find whether the given number is Prime number or Not.
    
    Input & Output Format:
    
    Input consists of one integer.
    
    Output consists of a string.
    
    If it is prime then display "Prime Number" or if it is not prime then display "Not a Prime"
    
    Sample Input:
    
    2
    
    Sample Output:
    
    Prime Number

---
+++ Prime.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class Prime
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int a;
+        int  flag=0;
+        a=sc.nextInt();
+        int m=a/2;
+        if(a<=1)
+        {
+            System.out.println("Not a Prime");
+        }
+        for(int i=2;i<m;i++)
+        {
+            if(a%i==0)
+            {
+                System.out.println("Not a prime");
+                flag=1;
+                break;
+            }
+        }
+        if(flag==0)
+        {
+            System.out.println("Prime Number"); 
+        }
+    }
+}
