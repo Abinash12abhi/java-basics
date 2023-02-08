69f2b95aad8f33059939246f6fe1fe10b9b2a3e3
Abinash12abhi <118423378+Abinash12abhi@users.noreply.github.com>
2023-02-08 22:25:26 +0530

    Series7
    Write a program to generate the following series --- 1, 2.0, 3.0, 6.0, 9.0, 18.0, 27.0,... 
    
    Input format: 
    
    The input containing an integer which denotes 'n' 
    
    Output format: 
    
    Print the series and refer the sample output for formatting
    
    Sample Input & Output:
    
    5
    
    1 2.0 3.0 6.0 9.0

---
+++ Series7.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class Series7
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        double a=sc.nextInt();
+        double b=1,c=2;
+        System.out.printf("%.0f %.1f ",b,c);
+        for(int i=1;i<a-1;i++)
+        {
+            if(i%2==1)
+            {
+                b=b*3;
+                System.out.printf("%.1f ",b);
+            }
+            else
+            {
+                c=c*3;
+                System.out.printf("%.1f ",c);
+            }
+        }
+    }
+}
