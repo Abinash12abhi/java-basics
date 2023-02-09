ce1a42c8a818b09a2e38d037067275db5651ced7
Abinash12abhi <118423378+Abinash12abhi@users.noreply.github.com>
2023-02-09 21:55:10 +0530

    Series
    Write a program to generate the following series 
    
    Sample Input:
    
    5
    
    Sample Output:
    
    3  2  11  14  27

---
+++ Series.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class Series
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        
+            int a;
+            
+            a=sc.nextInt();
+            for(int i=1;i<=a;i++)
+            {
+                if(i%2==0)
+                {
+                    System.out.print(((i*i)-2)+" ");
+                }
+                else
+                {
+                    System.out.print(((i*i)+2)+" ");
+                }
+            }
+        
+    }
+}
